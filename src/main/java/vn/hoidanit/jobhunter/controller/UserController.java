03a53e049699a8c621e2bba1409333f76672bc91
Bach Dao <ngocbach12a1@gmail.com>
2024-11-21 14:46:32 +0700

    video 28: @PathVariable

--- src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
@@ -1,5 +1,7 @@
 package vn.hoidanit.jobhunter.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,11 +18,17 @@ public class UserController {
     }
 
     // @GetMapping("/user/create")
-    @PostMapping("/user/create")
+    @PostMapping("/user")
     public User createNewUser(@RequestBody User postManUser) {
 
         User ericUser = this.userService.handleCreateUser(postManUser);
 
         return ericUser;
+    }
+
+    @DeleteMapping("/user/{id}")
+    public String deleteUser(@PathVariable("id") long id) {
+        this.userService.handleDeleteUser(id);
+        return "ericUser";
     }
 }
