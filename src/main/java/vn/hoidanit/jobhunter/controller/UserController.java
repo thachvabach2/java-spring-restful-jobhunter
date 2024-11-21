7b235d42e5a74a5e1ae2a80df857efd6ed7bcbaa
Bach Dao <ngocbach12a1@gmail.com>
2024-11-21 15:25:32 +0700

    video 32: Update User

--- src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -46,5 +47,11 @@ public class UserController {
     @GetMapping("/user")
     public List<User> getAllUsers() {
         return this.userService.fetchAllUser();
+    }
+
+    // update user
+    @PutMapping("/user")
+    public User updateUser(@RequestBody User user) {
+        return this.userService.handleUpdateUser(user);
     }
 }
