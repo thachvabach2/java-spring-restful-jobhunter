0cd6f5f2c2458a952fecffa8842fc2ac1b69be41
Bach Dao <ngocbach12a1@gmail.com>
2024-11-22 10:21:18 +0700

    video 40: Phạm vi của Exception

--- src/main/java/vn/hoidanit/jobhunter/controller/HelloController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/HelloController.java
@@ -1,12 +1,24 @@
 package vn.hoidanit.jobhunter.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import vn.hoidanit.jobhunter.service.error.IdInvalidException;
+
 @RestController
 public class HelloController {
+    @ExceptionHandler(value = IdInvalidException.class)
+    public ResponseEntity<String> handleIdException(IdInvalidException idException) {
+        return ResponseEntity.badRequest().body(idException.getMessage());
+    }
+
     @GetMapping("/")
-    public String getHelloWorld() {
+    public String getHelloWorld() throws IdInvalidException {
+        if (true) {
+            throw new IdInvalidException("check");
+        }
         return "Hello World (Hỏi Dân IT & Eric)";
     }
 
