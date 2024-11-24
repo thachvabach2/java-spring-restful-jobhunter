bb6a3dc9075c6b794056cde0e20131179ce2c050
Bach Dao <ngocbach12a1@gmail.com>
2024-11-24 16:36:43 +0700

    video 53: Debug Code (Extra)

--- src/main/java/vn/hoidanit/jobhunter/service/UserDetailsCustom.java
+++ src/main/java/vn/hoidanit/jobhunter/service/UserDetailsCustom.java
@@ -10,17 +10,20 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
 @Component("userDetailsService")
-public class UserDetailCustom implements UserDetailsService {
+public class UserDetailsCustom implements UserDetailsService {
 
     private final UserService userService;
 
-    public UserDetailCustom(UserService userService) {
+    public UserDetailsCustom(UserService userService) {
         this.userService = userService;
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         vn.hoidanit.jobhunter.domain.User user = this.userService.handleGetUserByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("Username/password không hợp lệ");
+        }
 
         return new User(
                 user.getEmail(),
