85d2746ebb22d212bdbc2e5ca4c5866288a2ef6c
Bach Dao <ngocbach12a1@gmail.com>
2024-11-25 21:27:42 +0700

    video 60: Giới thiệu dự án thực hành

--- src/main/java/vn/hoidanit/jobhunter/config/CustomAuthenticationEntryPoint.java
+++ src/main/java/vn/hoidanit/jobhunter/config/CustomAuthenticationEntryPoint.java
@@ -1,6 +1,7 @@
 package vn.hoidanit.jobhunter.config;
 
 import java.io.IOException;
+import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
@@ -33,7 +34,14 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
 
         RestResponse<Object> res = new RestResponse<Object>();
         res.setStatusCode(HttpStatus.UNAUTHORIZED.value());
-        res.setError(authException.getCause().getMessage());
+
+        // if authException.getCause() == null -> authException.getCause().getMessage(),
+        // else authException.getMessage()
+        String errorMessage = Optional.ofNullable(authException.getCause())
+                .map(Throwable::getMessage)
+                .orElse(authException.getMessage());
+        res.setError(errorMessage);
+
         res.setMessage("Token không hợp lệ (hết hạn, không đúng định dạng, hoặc không truyền JWT ở header)...");
 
         mapper.writeValue(response.getWriter(), res);
