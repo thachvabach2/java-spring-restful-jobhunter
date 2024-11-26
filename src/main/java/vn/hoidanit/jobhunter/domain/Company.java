f33609162869ca2368a3f4dec7a6d4981f2ae397
Bach Dao <ngocbach12a1@gmail.com>
2024-11-26 23:36:02 +0700

    video 70: Before Save Entity với JPA (Part 2)

--- src/main/java/vn/hoidanit/jobhunter/domain/Company.java
+++ src/main/java/vn/hoidanit/jobhunter/domain/Company.java
@@ -2,6 +2,8 @@ package vn.hoidanit.jobhunter.domain;
 
 import java.time.Instant;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -12,6 +14,7 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
+import vn.hoidanit.jobhunter.util.SecurityUtil;
 
 @Table(name = "companies")
 @Entity
@@ -32,6 +35,7 @@ public class Company {
 
     private String logo;
 
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss a", timezone = "GMT+7")
     private Instant createdAt;
 
     private Instant updatedAt;
@@ -42,7 +46,9 @@ public class Company {
 
     @PrePersist
     public void handleBeforeCreate() {
-        this.createdBy = "hoidanit";
+        this.createdBy = SecurityUtil.getCurrentUserLogin().isPresent() == true
+                ? SecurityUtil.getCurrentUserLogin().get()
+                : "";
         this.createdAt = Instant.now();
     }
 
