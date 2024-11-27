becc3a0751779efada4121286ca15dc9195c1ac3
Bach Dao <ngocbach12a1@gmail.com>
2024-11-27 11:28:52 +0700

    video 73: Query với Pagination

--- src/main/java/vn/hoidanit/jobhunter/service/CompanyService.java
+++ src/main/java/vn/hoidanit/jobhunter/service/CompanyService.java
@@ -3,9 +3,13 @@ package vn.hoidanit.jobhunter.service;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import vn.hoidanit.jobhunter.domain.Company;
+import vn.hoidanit.jobhunter.domain.dto.Meta;
+import vn.hoidanit.jobhunter.domain.dto.ResultPaginationDTO;
 import vn.hoidanit.jobhunter.repository.CompanyRepository;
 
 @Service
@@ -20,8 +24,22 @@ public class CompanyService {
         return this.companyRepository.save(company);
     }
 
-    public List<Company> handleGetCompany() {
-        return this.companyRepository.findAll();
+    public ResultPaginationDTO handleGetCompany(Pageable pageable) {
+        Page<Company> pageCompany = this.companyRepository.findAll(pageable);
+
+        ResultPaginationDTO rs = new ResultPaginationDTO();
+        Meta mt = new Meta();
+
+        mt.setPage(pageCompany.getNumber());
+        mt.setPageSize(pageCompany.getSize());
+        mt.setPages(pageCompany.getTotalPages());
+        mt.setTotal(pageCompany.getTotalElements());
+        rs.setMeta(mt);
+
+        List<Company> companies = pageCompany.getContent();
+        rs.setResult(companies);
+
+        return rs;
     }
 
     public Company handleUpdateCompany(Company company) {
