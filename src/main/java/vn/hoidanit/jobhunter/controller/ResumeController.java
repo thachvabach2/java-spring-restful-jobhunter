d93b08254d34edf564c1d5961a4bd879e37126cc
Bach Dao <ngocbach12a1@gmail.com>
2024-12-02 21:50:30 +0700

    video 105: CRUD Resume

---
+++ src/main/java/vn/hoidanit/jobhunter/controller/ResumeController.java
@@ -0,0 +1,98 @@
+package vn.hoidanit.jobhunter.controller;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.turkraft.springfilter.boot.Filter;
+
+import jakarta.validation.Valid;
+import vn.hoidanit.jobhunter.domain.Resume;
+import vn.hoidanit.jobhunter.domain.response.ResultPaginationDTO;
+import vn.hoidanit.jobhunter.domain.response.resume.ResCreateResumeDTO;
+import vn.hoidanit.jobhunter.domain.response.resume.ResGetResumeDTO;
+import vn.hoidanit.jobhunter.domain.response.resume.ResUpdateResumeDTO;
+import vn.hoidanit.jobhunter.service.ResumeService;
+import vn.hoidanit.jobhunter.util.annotation.ApiMessage;
+import vn.hoidanit.jobhunter.util.error.IdInvalidException;
+
+@RestController
+@RequestMapping("/api/v1")
+public class ResumeController {
+    private final ResumeService resumeService;
+
+    public ResumeController(ResumeService resumeService) {
+        this.resumeService = resumeService;
+
+    }
+
+    @PostMapping("/resumes")
+    @ApiMessage("Create a resume")
+    public ResponseEntity<ResCreateResumeDTO> createResume(@Valid @RequestBody Resume resume)
+            throws IdInvalidException {
+        // check id exist
+        boolean isIdExist = this.resumeService.checkResumeExistByUserAndJob(resume);
+        if (!isIdExist) {
+            throw new IdInvalidException("User id/Job id không tồn tại");
+        }
+
+        // create new resume
+        return ResponseEntity.status(HttpStatus.CREATED).body(this.resumeService.handleCreateResume(resume));
+    }
+
+    @PutMapping("/resumes")
+    @ApiMessage("Update a resume")
+    public ResponseEntity<ResUpdateResumeDTO> updateResume(@RequestBody Resume resume)
+            throws IdInvalidException {
+        // check id exist
+        Optional<Resume> reqResumeOptional = this.resumeService.fetchById(resume.getId());
+        if (reqResumeOptional.isEmpty()) {
+            throw new IdInvalidException("Resume với id = " + resume.getId() + " không tồn tại");
+        }
+
+        Resume reqResume = reqResumeOptional.get();
+        reqResume.setStatus(resume.getStatus());
+
+        return ResponseEntity.ok(this.resumeService.handleUpdateResume(reqResume));
+    }
+
+    @DeleteMapping("/resumes/{id}")
+    @ApiMessage("Delete a resume by id")
+    public ResponseEntity<Void> deleteResume(@PathVariable("id") long id) throws IdInvalidException {
+        Optional<Resume> reqResumeOptional = this.resumeService.fetchById(id);
+        if (reqResumeOptional.isEmpty()) {
+            throw new IdInvalidException("Resume với id = " + id + " không tồn tại");
+        }
+
+        this.resumeService.handleDeleteResume(id);
+        return ResponseEntity.ok(null);
+    }
+
+    @GetMapping("/resumes/{id}")
+    @ApiMessage("Fetch a resume by id")
+    public ResponseEntity<ResGetResumeDTO> getResume(@PathVariable("id") long id) throws IdInvalidException {
+        Optional<Resume> reqResumeOptional = this.resumeService.fetchById(id);
+        if (!reqResumeOptional.isPresent()) {
+            throw new IdInvalidException("Resume với id = " + id + " không tồn tại");
+        }
+
+        return ResponseEntity.ok(this.resumeService.handleGetResume(reqResumeOptional.get()));
+    }
+
+    @GetMapping("/resumes")
+    @ApiMessage("Fetch resumes with pagination")
+    public ResultPaginationDTO getAllResumes(@Filter Specification<Resume> spec, Pageable pageable) {
+        return this.resumeService.handleGetAllResumes(spec, pageable);
+    }
+}
