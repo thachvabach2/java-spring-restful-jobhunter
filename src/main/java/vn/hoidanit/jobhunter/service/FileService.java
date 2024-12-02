44ecaab24ed566bf525937e6baa102b4c480fdcf
Bach Dao <ngocbach12a1@gmail.com>
2024-12-02 12:23:38 +0700

    video 101: Upload File (Part 2)

--- src/main/java/vn/hoidanit/jobhunter/service/FileService.java
+++ src/main/java/vn/hoidanit/jobhunter/service/FileService.java
@@ -36,7 +36,7 @@ public class FileService {
 
     }
 
-    public void store(MultipartFile file, String folder) throws URISyntaxException, IOException {
+    public String store(MultipartFile file, String folder) throws URISyntaxException, IOException {
         // create unique filename
         String finalName = System.currentTimeMillis() + "-" + file.getOriginalFilename();
 
@@ -46,6 +46,8 @@ public class FileService {
             Files.copy(inputStream, path,
                     StandardCopyOption.REPLACE_EXISTING);
         }
+
+        return finalName;
     }
 
 }
