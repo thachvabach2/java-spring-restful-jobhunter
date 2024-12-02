0b1e03f098bfeecc7b6a6546a54e13050e1c84b5
Bach Dao <ngocbach12a1@gmail.com>
2024-12-02 13:05:10 +0700

    video 102: Download a File (Extra)

--- src/main/java/vn/hoidanit/jobhunter/controller/FileController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/FileController.java
@@ -1,14 +1,19 @@
 package vn.hoidanit.jobhunter.controller;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.time.Instant;
 import java.util.Arrays;
 import java.util.List;
 
-import org.apache.tomcat.util.http.fileupload.FileUploadException;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -59,5 +64,33 @@ public class FileController {
         ResUploadFileDTO res = new ResUploadFileDTO(uploadFile, Instant.now());
 
         return ResponseEntity.ok(res);
+    }
+
+    @GetMapping("/files")
+    @ApiMessage("Download a file")
+    public ResponseEntity<Resource> download(
+            @RequestParam(name = "fileName", required = false) String fileName,
+            @RequestParam(name = "folder", required = false) String folder)
+            throws StorageException, URISyntaxException, FileNotFoundException {
+        if (fileName == null || folder == null) {
+            throw new StorageException("Missing required params: (fileName or folder )");
+        }
+
+        // check file exist (and not a direction)
+        long fileLength = this.fileService.getFileLength(fileName, folder);
+        if (fileLength == 0) {
+            throw new StorageException("File with name = " + fileName + "not found");
+        }
+
+        // download a file
+        InputStreamResource resource = this.fileService.getResource(fileName, folder);
+
+        return ResponseEntity.ok()
+                // Content-Disposition: set tên file khi download
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileName + "\"")
+                // độ lớn của file -> frontend sử dụng để validate
+                .contentLength(fileLength)
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(resource);
     }
 }
