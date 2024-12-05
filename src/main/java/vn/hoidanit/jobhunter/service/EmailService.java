37f1fc3a25d8fe4c6238840bcefdd0eb56e1675e
Bach Dao <ngocbach12a1@gmail.com>
2024-12-05 10:23:55 +0700

    Send Email với Template (Part 1)

--- src/main/java/vn/hoidanit/jobhunter/service/EmailService.java
+++ src/main/java/vn/hoidanit/jobhunter/service/EmailService.java
@@ -1,18 +1,35 @@
 package vn.hoidanit.jobhunter.service;
 
+import java.nio.charset.StandardCharsets;
+
+import org.springframework.mail.MailException;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 
 @Service
 public class EmailService {
 
     private final MailSender mailSender;
+    private final JavaMailSender javaMailSender;
+    private final SpringTemplateEngine templateEngine;
 
-    public EmailService(MailSender mailSender) {
+    public EmailService(MailSender mailSender,
+            JavaMailSender javaMailSender,
+            SpringTemplateEngine templateEngine) {
         this.mailSender = mailSender;
+        this.javaMailSender = javaMailSender;
+        this.templateEngine = templateEngine;
     }
 
+    // send email with text (no html)
     public void sendSimpleEmail() {
         SimpleMailMessage msg = new SimpleMailMessage();
         msg.setTo("ngocbach.crack@gmail.com");
@@ -20,4 +37,27 @@ public class EmailService {
         msg.setText("Hello world from Spring Boot Email");
         this.mailSender.send(msg);
     }
+
+    // send email with text (html but hardcode)
+    public void sendEmailSync(String to, String subject, String content, boolean isMultipart, boolean isHtml) {
+        // Prepare message using a Spring helper
+        MimeMessage mimeMessage = this.javaMailSender.createMimeMessage();
+        try {
+            MimeMessageHelper message = new MimeMessageHelper(mimeMessage, isMultipart, StandardCharsets.UTF_8.name());
+            message.setTo(to);
+            message.setSubject(subject);
+            message.setText(content, isHtml);
+            this.javaMailSender.send(mimeMessage);
+        } catch (MailException | MessagingException e) {
+            System.out.println("ERROR SEND EMAIL: " + e);
+        }
+    }
+
+    // send email with text (html with template)
+    public void sendEmailFromTemplateSync(String to, String subject, String templateName) {
+        Context context = new Context();
+        String content = this.templateEngine.process(templateName, context);
+        this.sendEmailSync(to, subject, content, false, true);
+    }
+
 }
