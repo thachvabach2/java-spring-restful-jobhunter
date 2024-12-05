14a88c5e2c5d75da53ab215abe454dee4aa627ed
Bach Dao <ngocbach12a1@gmail.com>
2024-12-05 09:29:59 +0700

    Hello Word với Spring Email

--- src/main/java/vn/hoidanit/jobhunter/config/SecurityConfiguration.java
+++ src/main/java/vn/hoidanit/jobhunter/config/SecurityConfiguration.java
@@ -45,7 +45,8 @@ public class SecurityConfiguration {
                 "/",
                 "/api/v1/auth/login", "/api/v1/auth/refresh", "/api/v1/auth/register",
                 "/storage/**",
-                "/api/v1/companies/**", "/api/v1/jobs/**"
+                "/api/v1/companies/**", "/api/v1/jobs/**",
+                "/api/v1/email/**"
         };
 
         http
---
+++ src/main/java/vn/hoidanit/jobhunter/service/EmailService.java
@@ -0,0 +1,23 @@
+package vn.hoidanit.jobhunter.service;
+
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    private final MailSender mailSender;
+
+    public EmailService(MailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void sendSimpleEmail() {
+        SimpleMailMessage msg = new SimpleMailMessage();
+        msg.setTo("ngocbach.crack@gmail.com");
+        msg.setSubject("Tesing from Spring boot");
+        msg.setText("Hello world from Spring Boot Email");
+        this.mailSender.send(msg);
+    }
+}
